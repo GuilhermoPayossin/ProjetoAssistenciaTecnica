a24ad78f6643455df6e1d32646e407720fc57416
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-22 22:46:23 -0300

    Update

--- src/main/java/br/fiap/assistencia_tecnica/AssistenciaTecnicaApplication.java
+++ src/main/java/br/fiap/assistencia_tecnica/AssistenciaTecnicaApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssistenciaTecnicaApplication {
+public class
+AssistenciaTecnicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssistenciaTecnicaApplication.class, args);
